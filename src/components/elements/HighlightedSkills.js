ba55821d403b7cfc3e398a668c6760fa0a8fcd7f
Diego Couto <diego@couto.dev>
2020-05-12 14:55:31 -0300

    Update skills

--- src/components/elements/HighlightedSkills.js
+++ src/components/elements/HighlightedSkills.js
@@ -1,16 +1,16 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import Skill from './Skill';
 
 const HighlightedSkills = () => (
-  <Fragment>
-    <Skill name="Ruby on Rails" yearsOfExperience={7} />
-    <Skill name="React" yearsOfExperience={4} />
-    <Skill name="React Native" yearsOfExperience={2.5} />
-    <Skill name="GraphQL" yearsOfExperience={2} />
-    <Skill name="NodeJS" yearsOfExperience={4} />
-    <Skill name="AWS" yearsOfExperience={4} />
-  </Fragment>
+  <>
+    <Skill name="Ruby on Rails" yearsOfExperience={8} />
+    <Skill name="React" yearsOfExperience={5} />
+    <Skill name="React Native" yearsOfExperience={3.5} />
+    <Skill name="GraphQL" yearsOfExperience={3} />
+    <Skill name="NodeJS" yearsOfExperience={5} />
+    <Skill name="AWS" yearsOfExperience={5} />
+  </>
 );
 
 export default HighlightedSkills;
